b4ec83d82ab399b21dcf247aeee6502bd5b2f6b9
Sayan Atta <sayanatta10@gmail.com>
2022-08-26 00:56:51 +0530

    feat: sigin api done

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -20,4 +20,27 @@ const signup = async (req, res) => {
   }
 };
 
-export default { signup };
+const signin = async (req, res) => {
+  try {
+    const user = await UserModel.findOne({ email: req.body.email });
+    if (!!user) {
+      const validPassword = await bcrypt.compare(
+        req.body.password,
+        user.password
+      );
+      if (validPassword) {
+        res
+          .status(200)
+          .json({ success: true, msg: "Login Successful", data: user });
+      } else {
+        res.status(400).json({ success: false, msg: "Invalid Password" });
+      }
+    } else {
+      res.status(400).json({ success: false, msg: "Oppsss!!! User not found" });
+    }
+  } catch (e) {
+    res.status(500).json({ success: false, msg: res });
+  }
+};
+
+export default { signup, signin };
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,5 +3,6 @@ import AuthController from "../controllers/AuthController.js";
 const router = express.Router();
 
 router.post("/sign-up", AuthController.signup);
+router.post("/sign-in", AuthController.signin);
 
 export default router;
